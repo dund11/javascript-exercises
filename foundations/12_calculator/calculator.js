c3f59094bda6edcb3616dea2d2ed6a2b13ff5993
dund11 <252978918+dund11@users.noreply.github.com>
2026-08-18 18:50:53 +0700

    Complete calculator and palindromes exercises

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let i=0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i=0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let total = 1;
+  for (let i=0; i < exponent; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  for (let i=1; i <= n; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversed = cleanStr.split("").reverse().join("");
+    
+    return cleanStr === reversed;
 };
-
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes; 
+/*console.log(palindromes('racecar')); 
+console.log(palindromes('tacos'));
+console.log(palindromes('A car, a man, a maraca.')); */
