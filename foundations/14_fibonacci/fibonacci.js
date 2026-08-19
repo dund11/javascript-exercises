27e51644b73677c60a3f765e9a27021e1d62f1cc
dund11 <252978918+dund11@users.noreply.github.com>
2026-08-19 18:56:28 +0700

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+ if (n === 0) {
+ return 0;
+ }
+ if (n < 0) {
+    return "OOPS";
+ } 
+ let a = 0;
+ let b = 1;
 
+ for (let i = 2; i <= n; i++) {
+    let next = a + b;
+    a = b;
+    b = next;
+ }
+    return b;
 };
 
 // Do not edit below this line
